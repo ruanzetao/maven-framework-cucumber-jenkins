19cac12b696852c20ab39c405a7f784b8546352f
ruanzetao <ruanzetao>
2022-10-16 12:44:07 +0700

    Cucumber: init Project

---
+++ src/test/java/features/verifyFacebook.feature
@@ -0,0 +1,9 @@
+
+Feature: Facebook Login page
+  I want to use this template for my feature file
+
+  @displayed
+  Scenario: Verify login page
+    Given Open Facebook application
+    Then Verify Email texbox is displayed
+    And Verify Password textbox is displayed
---
+++ src/test/java/stepDefinitions/FacebookSteps.java
@@ -0,0 +1,25 @@
+package stepDefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class FacebookSteps {
+	@Given("^Open Facebook application$")
+	public void openFacebookApplication() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^Verify Email texbox is displayed$")
+	public void verifyEmailTexboxIsDisplayed() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^Verify Password textbox is displayed$")
+	public void verifyPasswordTextboxIsDisplayed() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+}
